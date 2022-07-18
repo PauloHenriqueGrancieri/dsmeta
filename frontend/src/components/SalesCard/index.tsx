66a94aac07800e3d37c3e9cdbc82e7546f5bae81
Paulo Henrique Grancieri <paulohenriquegrancieri@gmail.com>
2022-07-17 21:27:25 -0300

    Toast

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -1,52 +1,55 @@
-import axios from "axios"
-import { useEffect, useState } from "react"
-import DatePicker from "react-datepicker"
-import "react-datepicker/dist/react-datepicker.css"
-import { Sale } from "../../models/sale"
-import { BASE_URL } from "../../utils/request"
-import NotificationButton from '../NotificationButton'
-import './styles.css'
+import NotificationButton from '../NotificationButton';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import './styles.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { BASE_URL } from '../../utils/request';
+import { Sale } from '../../models/sale';
 
 function SalesCard() {
 
-    const min = new Date(new Date().setDate(new Date().getDate() - 365))
-    const max = new Date()
+    const min = new Date(new Date().setDate(new Date().getDate() - 365));
+    const max = new Date();
 
-    const [minDate, setMinDate] = useState(min)
-    const [maxDate, setMaxDate] = useState(max)
+    const [minDate, SetMinDate] = useState(min);
+    const [maxDate, setMaxDate] = useState(max);
 
     const [sales, setSales] = useState<Sale[]>([]);
 
     useEffect(() => {
 
         const dmin = minDate.toISOString().slice(0, 10);
-        const dmax = minDate.toISOString().slice(0, 10);
+        const dmax = maxDate.toISOString().slice(0, 10);
 
         axios.get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`)
             .then(response => {
-                setSales(response.data.content)
-            })
-    }, [minDate, maxDate]);
+                setSales(response.data.content);
+            });
+    }, [minDate, maxDate])
 
     return (
         <div className="dsmeta-card">
             <h2 className="dsmeta-sales-title">Vendas</h2>
-            <div className="dsmeta-form-control-container">
-                <DatePicker
-                    selected={minDate}
-                    onChange={(date: Date) => setMinDate(date)}
-                    className="dsmeta-form-control"
-                    dateFormat="dd/MM/yyyy"
-                />
+            <div>
+                <div className="dsmeta-form-control-container">
+                    <DatePicker
+                        selected={minDate}
+                        onChange={(date: Date) => SetMinDate(date)}
+                        className="dsmeta-form-control"
+                        dateFormat="dd/MM/yyyy"
+                    />
+                </div>
+                <div className="dsmeta-form-control-container">
+                    <DatePicker
+                        selected={maxDate}
+                        onChange={(date: Date) => setMaxDate(date)}
+                        className="dsmeta-form-control"
+                        dateFormat="dd/MM/yyyy"
+                    />
+                </div>
             </div>
-            <div className="dsmeta-form-control-container">
-                <DatePicker
-                    selected={maxDate}
-                    onChange={(date: Date) => setMaxDate(date)}
-                    className="dsmeta-form-control"
-                    dateFormat="dd/MM/yyyy"
-                />
-            </div>
+
             <div>
                 <table className="dsmeta-sales-table">
                     <thead>
@@ -69,7 +72,7 @@ function SalesCard() {
                                     <td>{sale.sellerName}</td>
                                     <td className="show992">{sale.visited}</td>
                                     <td className="show992">{sale.deals}</td>
-                                    <td>{sale.amount.toFixed(2)}</td>
+                                    <td>R$ {sale.amount.toFixed(2)}</td>
                                     <td>
                                         <div className="dsmeta-red-btn-container">
                                             <NotificationButton saleId={sale.id} />
@@ -79,10 +82,13 @@ function SalesCard() {
                             )
                         })}
                     </tbody>
+
                 </table>
             </div>
+
         </div>
     )
+
 }
 
 export default SalesCard;
